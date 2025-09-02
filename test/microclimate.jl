3280513fcfd592acb211b6e2be545fa277adcdbf
mrke <mrke@unimelb.edu.au>
2025-09-02 16:34:11 +1000

    added code to compute microclimate via runmicro and use the new outputs, including direct and diffuse solar

--- test/microclimate.jl
+++ test/microclimate.jl
@@ -26,23 +26,9 @@ lon = 140.0°
 elev = 10.0m
 days = [15, 45]*1.0
 hours = collect(0.:1:24.)
+heights = [1.0,]u"cm"
 
-# compute solar radiation
-solrad_out = solrad(;
-    days,       # days of year
-    hours,      # hours of day
-    lat,        # latitude (degrees)
-    elev,
-    )
-# extract results, skipping 25th values
-skip25 = setdiff(1:length(solrad_out.Zenith), 25:25:length(solrad_out.Zenith))
-solrad_out.Zenith[solrad_out.Zenith.>90u"°"] .= 90u"°"
-Q_sol = solrad_out.Global[skip25]
-Zenith = solrad_out.Zenith[skip25]
-Q_dir = solrad_out.Direct[skip25]
-Q_dif = solrad_out.Scattered[skip25]
-
-# define weather and soil moisture
+# define daily weather and soil moisture
 TIMINS = [0, 0, 1, 1] # time of minima for air temp, wind, humidity and cloud cover (h), air & wind mins relative to sunrise, humidity and cloud cover mins relative to solar noon
 TIMAXS = [1, 1, 0, 0] # time of maxima for air temp, wind, humidity and cloud cover (h), air temp & wind maxs relative to solar noon, humidity and cloud cover maxs relative to sunrise
 TMINN = [10.0, 8.0]u"°C" # minimum air temperatures (°C)
@@ -53,36 +39,15 @@ WNMINN = [0.1, 0.2]u"m/s" # min wind speed (m/s)
 WNMAXX = [1.0, 1.4]u"m/s" # max wind speed (m/s)
 CCMINN = [20.0, 23.0] # min cloud cover (%)
 CCMAXX = [90.0, 100.0] # max cloud cover (%)
-SoilMoist = [0.0, 0.0]
+SoilMoist = [0.2, 0.2] # fractional
 minshade = 0.0
 maxshade = 90.0
 
-# interpolate air temperature to hourly
-TAIRs, WNs, RHs, CLDs = hourly_vars(
-    TMINN,
-    TMAXX,
-    WNMINN,
-    WNMAXX,
-    RHMINN,
-    RHMAXX,
-    CCMINN,
-    CCMAXX,
-    solrad_out,
-    TIMINS,
-    TIMAXS
-)
-RHs[RHs.>100] .= 100
-CLDs[CLDs.>100] .= 100
-# skip 25th values
-TAIRs = TAIRs[skip25]
-WNs = WNs[skip25]
-RHs = RHs[skip25]
-CLDs = CLDs[skip25]
-
-# compute soil and sky temperature in minshade environment
+# run microclimate model in minshade environment
 micro_minshade = runmicro(;
     lat,
     elev,
+    heights,
     days,
     hours,
     TMINN,
@@ -97,13 +62,11 @@ micro_minshade = runmicro(;
     SHADES = fill(minshade, length(TAIRs)),
 )
 
-T_soils_minshade =  micro_minshade.T_soils
-T_skys_minshades = micro_minshade.T_skys
-
-# compute soil and sky temperature in minshade environment
+# run microclimate model in maxshade environment
 micro_maxshade = runmicro(;
     lat,
     elev,
+    heights,
     days,
     hours,
     TMINN,
@@ -117,35 +80,35 @@ micro_maxshade = runmicro(;
     SoilMoist,
     SHADES = fill(maxshade, length(TAIRs)),
 )
-T_soils_maxshade =  micro_maxshade.T_soils
-T_skys_maxshades = micro_maxshade.T_skys
 
 env_minshade = EnvironmentalVarsVec(
-    T_air=K.(TAIRs),
-    T_sky=T_skys_minshades,
-    T_sub=T_skys_minshades[:, 1],
-    rh=RHs,
-    vel=WNs,
-    Q_sol=Q_sol .* (1.0-minshade/100.0),
-    Q_dir=Q_dir .* (1.0-minshade/100.0),
-    Q_dif=Q_dif .* (1.0-minshade/100.0),
-    zen=Zenith
+    T_air = K.(micro_minshade.airtemperature[:, 2]), # second column is first node above surface
+    T_sky = micro_minshade.skytemperature,
+    T_sub = micro_minshade.soiltemperature[:, 1], # surface temperature
+    rh = micro_minshade.relativehumidity[:, 2], # second column is first node above surface
+    vel = micro_minshade.windspeed[:, 2], # second column is first node above surface
+    Q_sol = micro_minshade.globalsolar .* (1.0-minshade/100.0),
+    Q_dir = micro_minshade.directsolar .* (1.0-minshade/100.0),
+    Q_dif = micro_minshade.diffusesolar .* (1.0-minshade/100.0),
+    zen = micro_minshade.zenithangle
 )
 
 env_maxshade = EnvironmentalVarsVec(
-    T_air=K.(TAIRs),
-    T_sky=T_skys_maxshades,
-    T_sub=T_skys_maxshades[:, 1],
-    rh=RHs,
-    vel=WNs,
-    Q_sol=Q_sol .* (1.0-maxshade/100.0),
-    Q_dir=Q_dir .* (1.0-maxshade/100.0),
-    Q_dif=Q_dif .* (1.0-maxshade/100.0),
-    zen=Zenith
+    T_air = K.(micro_maxshade.airtemperature[:, 2]),
+    T_sky =micro_maxshade.skytemperature,
+    T_sub = micro_maxshade.soiltemperature[:, 1],
+    rh = micro_maxshade.relativehumidity[:, 2],
+    vel = micro_maxshade.windspeed[:, 2],
+    Q_sol = micro_maxshade.globalsolar .* (1.0-minshade/100.0),
+    Q_dir = micro_maxshade.directsolar .* (1.0-minshade/100.0),
+    Q_dif = micro_maxshade.diffusesolar .* (1.0-minshade/100.0),
+    zen = micro_maxshade.zenithangle
 )
 
-environment = env_maxshade
+# set shade
+environment = env_minshade
 
+# compute body temperature
 balances = map(1:n) do i
     env_i = EnvironmentalVars(
         T_air   = environment.T_air[i],
