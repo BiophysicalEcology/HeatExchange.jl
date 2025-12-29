09a563f0fcff3e52b17861483fe2a52d11d07a4f
mrke <mrke@unimelb.edu.au>
2025-12-30 00:43:02 +1100

    add workflows and update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,5 @@ Aqua.test_project_extras(HeatExchange)
 Aqua.test_deps_compat(HeatExchange)
 Aqua.test_project_toml_formatting(HeatExchange)
 
-@safetestset "biophysics" begin include("biophysics.jl") end
-@safetestset "geometry" begin include("geometry.jl") end
-@safetestset "environment" begin include("environment.jl") end
-@safetestset "organism" begin include("geometry.jl") end
+@safetestset "endotherm" begin include("endotherm.jl") end
+@safetestset "ectotherm" begin include("ectotherm.jl") end
