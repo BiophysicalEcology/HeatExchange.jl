c94cb5a5e3605bee713b72d66d841ee341b7ead5
mrke <mrke@unimelb.edu.au>
2025-12-30 23:59:16 +1100

    fix metabolism test, remove organism.jl in test

--- test/metabolism.jl
+++ test/metabolism.jl
@@ -5,9 +5,9 @@ Q_metab = 1.0u"W"
 O2_consumption = 1.0u"ml/hr"
 rq = 0.8
 
-u"ml/hr"(Joules_to_O2(Typical(), Q_metab))
+u"ml/hr"(Joules_to_O2(Typical(), Q_metab, rq))
 u"ml/hr"(Joules_to_O2(Q_metab))
-u"J/hr"(O2_to_Joules(Typical(), O2_consumption))
+u"J/hr"(O2_to_Joules(Typical(), O2_consumption, rq))
 u"J/hr"(O2_to_Joules(1.0u"ml/hr"))
 
 u"ml/hr"(Joules_to_O2(Kleiber1961(), Q_metab, rq))
