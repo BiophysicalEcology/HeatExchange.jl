00a5f3578c1559c879dd294520bc079a97a7b0ca
mrke <mrke@unimelb.edu.au>
2025-12-31 00:06:02 +1100

    fix environment.jl  test

--- test/environment.jl
+++ test/environment.jl
@@ -3,9 +3,8 @@ using ModelParameters
 using Unitful
 
 env_pars = EnvironmentalPars(
-    α_substrate = 0.8,
-    shade = 0.0,
-    ϵ_substrate = 1.0,
+    α_ground = 0.8,
+    ϵ_ground = 1.0,
     ϵ_sky = 1.0,
     elevation = 0.0u"m",
     fluid = 0,
@@ -16,19 +15,18 @@ env_pars = EnvironmentalPars(
 env_params = Model(env_pars)
 env_pars = stripparams(env_params)
 
-env_vars = EnvironmentalVars(
+env_vars = EnvironmentalVars(;
     T_air = (273.15+20.0)u"K",
-    T_air_reference = (273.15+20.0)u"K",
     T_sky = (273.15-5.0)u"K",
     T_ground = (273.15+30.0)u"K",
     T_substrate = (273.15+20.0)u"K",
     T_bush = (273.15+20.0)u"K",
-    T_vegation = (273.15+20.0)u"K",
     rh = 0.05,
     wind_speed = 1.0u"m/s",
     P_atmos = 101325.0u"Pa",
     zenith_angle = 20.0u"°",
+    global_radiation = 1000.0u"W/m^2",
     k_substrate = 0.5u"W/m/K",
-    global_solar = 1000.0u"W/m^2",
-    fraction_diffuse = 964.177772475912u"W/m^2",
+    diffuse_fraction = 0.15,
+    shade = 0.0,
 )
