8e69cedc11f583e43812d00f603edaadf51d6ac3
Rafael Schouten <rafaelschouten@gmail.com>
2026-01-08 00:00:47 +1100

    fix Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,13 @@ using HeatExchange
 using SafeTestsets
 using Aqua
 
-Aqua.test_unbound_args(HeatExchange)
-Aqua.test_stale_deps(HeatExchange)
-Aqua.test_undefined_exports(HeatExchange)
-Aqua.test_project_extras(HeatExchange)
-Aqua.test_deps_compat(HeatExchange)
-Aqua.test_project_toml_formatting(HeatExchange)
+@testset "Quality assurance" begin
+    Aqua.test_unbound_args(HeatExchange)
+    Aqua.test_stale_deps(HeatExchange)
+    Aqua.test_undefined_exports(HeatExchange)
+    Aqua.test_project_extras(HeatExchange)
+    Aqua.test_deps_compat(HeatExchange)
+end
 
 @safetestset "biophysics" begin include("biophysics.jl") end
 @safetestset "geometry" begin include("geometry.jl") end
