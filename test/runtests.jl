0d5397c524d1219f202b8c9a9fbd01db32b23974
Rafael Schouten <rafaelschouten@gmail.com>
2026-01-08 22:09:57 +1100

    dependency cleanup  (#14)
    * remove unused dependencies
    
    * add FluidProperties in sources
    
    * add compat bounds
    
    * fix Aqua
    
    * using Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,15 @@
 using HeatExchange
-using SafeTestsets
 using Aqua
+using SafeTestsets
+using Test
 
-Aqua.test_unbound_args(HeatExchange)
-Aqua.test_stale_deps(HeatExchange)
-Aqua.test_undefined_exports(HeatExchange)
-Aqua.test_project_extras(HeatExchange)
-Aqua.test_deps_compat(HeatExchange)
-Aqua.test_project_toml_formatting(HeatExchange)
+@testset "Quality assurance" begin
+    Aqua.test_unbound_args(HeatExchange)
+    Aqua.test_stale_deps(HeatExchange)
+    Aqua.test_undefined_exports(HeatExchange)
+    Aqua.test_project_extras(HeatExchange)
+    Aqua.test_deps_compat(HeatExchange)
+end
 
 @safetestset "biophysics" begin include("biophysics.jl") end
 @safetestset "geometry" begin include("geometry.jl") end
