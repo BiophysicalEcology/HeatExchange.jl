6be44b922c14572213e99f78490716c4749e9ff1
mrke <mrke@unimelb.edu.au>
2026-04-21 22:36:35 +1000

    tweaks to respiration.jl

--- src/respiration.jl
+++ src/respiration.jl
@@ -38,7 +38,7 @@ function respiration(
     fCO2 = gas_fractions.carbon_dioxide
     fN2 = gas_fractions.nitrogen
     # adjust O2 to ensure sum to 1
-    if fO2 + fCO2 + fN2 != 1
+    if !isapprox(fO2 + fCO2 + fN2, 1; atol=1e-6)
         fO2 = 1 - (fN2 + fCO2)
     end
 
@@ -49,21 +49,22 @@ function respiration(
 
     # converting standard temperature and pressure -> vol. of O2 at animal lung temperature, atm. press.
     O2_partial_pressure = atmospheric_pressure * fO2
-    oxygen_flow = (oxygen_flow_standard * O2_partial_pressure / 273.15u"K") * (lung_temperature / O2_partial_pressure)
+    oxygen_flow = oxygen_flow_standard * (lung_temperature / 273.15u"K") * (101325u"Pa" / atmospheric_pressure)
     #n = PV/RT (ideal gas law: number of moles from press,vol,temp)
     oxygen_consumed = uconvert(u"mol/s", atmospheric_pressure * oxygen_flow / (Unitful.R * lung_temperature)) # mol O2 consumed
     # moles/s of O2, N2, dry air at entrance [air flow = f(O2 consumption)]
     oxygen_in = oxygen_consumed / oxygen_extraction_efficiency # actual oxygen flow in (moles/s), accounting for efficiency of extraction
     nitrogen_in = oxygen_in * (fN2 / fO2) #  actual nitrogen flow in (moles/s), accounting for efficiency of extraction
-    air_flow = oxygen_flow / fO2 # air flow
-    co2_flow = fCO2 * air_flow # CO2 flow
+    air_flow_raw = oxygen_flow / fO2 # air flow
+    co2_flow = fCO2 * air_flow_raw # CO2 flow
     carbon_dioxide_in = atmospheric_pressure * co2_flow / (Unitful.R * lung_temperature)
     air_in = (oxygen_in + nitrogen_in + carbon_dioxide_in) * pant
     air_flow = uconvert(u"m^3/s", (air_in * Unitful.R * 273.15u"K" / 101325u"Pa")) # air volume at standard temperature and pressure (m3/s)
     # computing the vapor pressure at saturation for the subsequent calculation of
     # actual moles of water based on actual relative humidity
     saturation_vapour_pressure = vapour_pressure(air_temperature)
-    water_in = air_in * (saturation_vapour_pressure * relative_humidity) / (atmospheric_pressure - saturation_vapour_pressure * relative_humidity)
+    water_in = air_in * (saturation_vapour_pressure * relative_humidity) / 
+        max(atmospheric_pressure - saturation_vapour_pressure * relative_humidity, 1e-6u"Pa")
     # moles at exit
     oxygen_out = oxygen_in - oxygen_consumed # remove consumed oxygen from the total
     nitrogen_out = nitrogen_in
@@ -95,11 +96,6 @@ function respiration(
     # (edwards & haines 1978. j. comp. physiol. 128: 177-184 in welch 1980)
     # for a 0.01 kg animal, the max. rate would be 1.67 x 10^-6 g/s
     respiration_mass = min(respiration_mass, (2.22E-03 * ustrip(u"kg", mass) * 15)u"g/s")
-
-    # get latent heat of vapourisation and compute heat exchange due to respiration
-    latent_heat_vaporisation = enthalpy_of_vaporisation(lung_temperature)
-    # heat loss by breathing (J/s)=(J/kg)*(kg/s)
-    respiration_heat_flow = uconvert(u"W", latent_heat_vaporisation * respiration_mass)
 
     # get latent heat of vapourisation and compute heat exchange due to respiration
     latent_heat_vaporisation = enthalpy_of_vaporisation(lung_temperature)
